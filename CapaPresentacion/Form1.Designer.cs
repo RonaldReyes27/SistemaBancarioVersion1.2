db6d8af5c03988596c302fc410a1be15e3bbba7c
Ronald Reyes <ronaldalberto363@gmail.com>
2025-07-08 03:41:04 -0400

    cambios en el forms

--- CapaPresentacion/Form1.Designer.cs
+++ CapaPresentacion/Form1.Designer.cs
@@ -28,18 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            sqlConnection1 = new Microsoft.Data.SqlClient.SqlConnection();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(970, 40);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Menu_black_garra;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(39, 32);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // sqlConnection1
+            // 
+            sqlConnection1.AccessTokenCallback = null;
+            sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Banco Eurux | Menu";
+            label1.Click += label1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1288, 699);
+            ClientSize = new Size(970, 531);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Microsoft.Data.SqlClient.SqlConnection sqlConnection1;
+        private Label label1;
     }
 }
---
+++ Presentacion/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Presentacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            nightControlBox1 = new ReaLTaiizor.Controls.NightControlBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            panel3 = new Panel();
+            button2 = new Button();
+            panel4 = new Panel();
+            button3 = new Button();
+            panel5 = new Panel();
+            button4 = new Button();
+            flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.FromArgb(23, 24, 29);
+            flowLayoutPanel1.Controls.Add(panel2);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 44);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(258, 690);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(66, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(183, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Banco Eurux | Menu";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Menu_blue;
+            pictureBox1.Location = new Point(3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(57, 36);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // nightControlBox1
+            // 
+            nightControlBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            nightControlBox1.BackColor = Color.Transparent;
+            nightControlBox1.CloseHoverColor = Color.FromArgb(199, 80, 80);
+            nightControlBox1.CloseHoverForeColor = Color.White;
+            nightControlBox1.DefaultLocation = true;
+            nightControlBox1.DisableMaximizeColor = Color.FromArgb(105, 105, 105);
+            nightControlBox1.DisableMinimizeColor = Color.FromArgb(105, 105, 105);
+            nightControlBox1.EnableCloseColor = Color.FromArgb(160, 160, 160);
+            nightControlBox1.EnableMaximizeButton = true;
+            nightControlBox1.EnableMaximizeColor = Color.FromArgb(160, 160, 160);
+            nightControlBox1.EnableMinimizeButton = true;
+            nightControlBox1.EnableMinimizeColor = Color.FromArgb(160, 160, 160);
+            nightControlBox1.Location = new Point(974, 7);
+            nightControlBox1.MaximizeHoverColor = Color.FromArgb(15, 255, 255, 255);
+            nightControlBox1.MaximizeHoverForeColor = Color.White;
+            nightControlBox1.MinimizeHoverColor = Color.FromArgb(15, 255, 255, 255);
+            nightControlBox1.MinimizeHoverForeColor = Color.White;
+            nightControlBox1.Name = "nightControlBox1";
+            nightControlBox1.Size = new Size(139, 31);
+            nightControlBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(nightControlBox1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1125, 44);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(255, 50);
+            panel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(23, 24, 29);
+            button1.ForeColor = Color.White;
+            button1.Image = Properties.Resources.Administrator_Male__rff;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(-19, -19);
+            button1.Name = "button1";
+            button1.Padding = new Padding(25, 0, 0, 0);
+            button1.Size = new Size(291, 91);
+            button1.TabIndex = 3;
+            button1.Text = "         Dashboard";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button2);
+            panel3.Location = new Point(478, 313);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(255, 50);
+            panel3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(23, 24, 29);
+            button2.ForeColor = Color.White;
+            button2.Image = Properties.Resources.Settings;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(-19, -19);
+            button2.Name = "button2";
+            button2.Padding = new Padding(25, 0, 0, 0);
+            button2.Size = new Size(291, 91);
+            button2.TabIndex = 3;
+            button2.Text = "         Configuracion";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(button3);
+            panel4.Location = new Point(478, 414);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(255, 50);
+            panel4.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(23, 24, 29);
+            button3.ForeColor = Color.White;
+            button3.Image = Properties.Resources.Administrator_Male__rff;
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(-19, -19);
+            button3.Name = "button3";
+            button3.Padding = new Padding(25, 0, 0, 0);
+            button3.Size = new Size(291, 91);
+            button3.TabIndex = 3;
+            button3.Text = "         Logout";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(button4);
+            panel5.Location = new Point(478, 506);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(255, 50);
+            panel5.TabIndex = 6;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(23, 24, 29);
+            button4.ForeColor = Color.White;
+            button4.Image = Properties.Resources.Administrator_Male__rff;
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(-19, -19);
+            button4.Name = "button4";
+            button4.Padding = new Padding(25, 0, 0, 0);
+            button4.Size = new Size(291, 91);
+            button4.TabIndex = 3;
+            button4.Text = "         About";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1155, 734);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Padding = new Padding(0, 0, 30, 0);
+            Text = "Form1";
+            flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private ReaLTaiizor.Controls.NightControlBox nightControlBox1;
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private Panel panel3;
+        private Button button2;
+        private Panel panel4;
+        private Button button3;
+        private Panel panel5;
+        private Button button4;
+    }
+}
